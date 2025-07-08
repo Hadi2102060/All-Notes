81dd66a44deb0d13e201e394ae694edc5141a728
Hadi2102060 <ug2102060@cse.pstu.ac.bd>
2025-07-09 01:19:25 +0600

    update

--- Python_course/02_chapter/03_operators.py
+++ Python_course/02_chapter/03_operators.py
@@ -14,3 +14,28 @@ b = 4  # b is operand and "=" is an Assignment operator
 c = a + b   # using arithmetic operator
 
 print (c)
+
+
+
+
+# Logical Operator 
+
+
+e = True or False 
+
+
+ # TruthTable of "or "
+
+
+print("True or False is : ", True or False)
+print("True or False is : ", True or True)
+print("True or False is : ", False or True)
+print("True or False is : ", False or False)
+
+
+# TruthTable of "and "
+
+print("True and False is : ", True and False)
+print("True and False is : ", True and True)
+print("True and False is : ", False and True)
+print("True and False is : ", False and False)
