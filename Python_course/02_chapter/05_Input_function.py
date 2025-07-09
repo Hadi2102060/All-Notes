df106a36513749bd23fa641a10a62201e804416a
Hadi2102060 <ug2102060@cse.pstu.ac.bd>
2025-07-09 12:53:55 +0600

    update

---
+++ Python_course/02_chapter/05_Input_function.py
@@ -0,0 +1,7 @@
+a = int(input("Enter the first number : "))
+b = int(input("Enter the second number: "))
+
+print("The first number a is : ")
+print("The first number b is : ")
+
+print("The sum is : ",a + b)
