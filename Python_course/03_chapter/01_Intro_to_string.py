b19247b821fcd5f7addc160c6ba5a589fb67bd17
Hadi2102060 <ug2102060@cse.pstu.ac.bd>
2025-08-03 12:34:53 +0600

    update note

---
+++ Python_course/03_chapter/01_Intro_to_string.py
@@ -0,0 +1,23 @@
+# concept _01 =  We can write string in three ways like in the below: 
+
+a = 'Hadi'             #way number - 01
+a = "Hadi"             # way number - 02
+a = '''Hadi'''          # way number - 03
+
+print(a)
+
+# concept_01 = we can not change any character in existing string like str = "Hadi" we can not change the character a in Hadi
+# For this problem solve to create new string
+
+# string devided formate:
+
+name = "Hadi"
+'''
+devide = name[index_start, index_end]
+        this is included          this is not included 
+
+        '''
+devide = name[0:2]           # ekhane start hobe 0 theke and end hobe 1 e but 2 add hobe nh
+character = name[0]
+lenth = len(name)
+print(devide, character, lenth)
