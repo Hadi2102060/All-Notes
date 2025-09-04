6cf3b0c9e140baca5b43e347aba7228fe1edf626
Hadi2102060 <ug2102060@cse.pstu.ac.bd>
2025-09-04 22:12:38 +0600

    update

---
+++ Dart_language/firstprogram/lib/dart_constant.dart
@@ -0,0 +1,35 @@
+/*
+
+.................. Dart Constant.......................
+
+
+constant is a keyword whose value cannot be changed after using it.
+
+
+int a = 10; // variable
+a = 20; // value can be changed and it is right way
+
+
+But .......
+
+const b =30; // constant
+b =40 ; // value cannot be changed and it is wrong way
+// Also constant variable must be initialized at the time of declaration.
+// not support only declaration like  this (const int a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */
