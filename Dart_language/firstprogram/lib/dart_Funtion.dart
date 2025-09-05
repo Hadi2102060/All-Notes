a82ceeaadc536eda36567195287d98a4b4c040c9
Hadi2102060 <ug2102060@cse.pstu.ac.bd>
2025-09-05 22:19:26 +0600

    update

---
+++ Dart_language/firstprogram/lib/dart_Funtion.dart
@@ -0,0 +1,46 @@
+/*
+
+....................Dart Function...................
+
+a function is a block of code that performs a specific task. It is a reusable piece of code that can be called multiple times throughout a program.
+
+
+function bolte bujhai je amra jodi kono kaj bar bar korte chai tahole sei kaj ta ke amra akta function er moddhe rakhbo and jokhon proyojon hobe tokhon oi function ta ke call kore sei kaj ta ke abar abar korte parbo.
+
+For example: 
+
+
+        void main(){
+        stdout.write("Enter first  number :");
+        int num1 = int.parse(stdin.readLineSync()!);
+
+        stdout.write("Enter second number :  ");
+        int num2 = int.parse(stdin.readLineSync()!);
+
+        int sum = num1+num2;
+        stdout.write(sum);
+        
+        }
+
+
+There are some types of function : 
+
+    1. void function   // can not return value
+    2. int function    // return integer value
+    3. string function   /// return string value
+    4. double function  // return double value
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */
