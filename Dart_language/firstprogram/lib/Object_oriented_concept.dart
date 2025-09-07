dc82671a9706e263e37220912034c83b1fd8b82e
Hadi2102060 <ug2102060@cse.pstu.ac.bd>
2025-09-07 20:50:13 +0600

    update

---
+++ Dart_language/firstprogram/lib/Object_oriented_concept.dart
@@ -0,0 +1,89 @@
+/*
+.................Dart Object -oriented programming...........................
+
+
+//OOPs is the meaning of Object oriented programming or Object oriented paradism.
+
+Before object oriented programming we use a lot of line in code 
+
+but when i use object oriented programming(OOPs) then the less code that i write .
+
+
+// The concept of Object oriented programming that -----
+
+
+      1. class
+      2. object 
+      3. abstraction
+      4. Encapsulation
+      5. Inheritance
+
+
+ 1. Class : 
+    
+      class means Structure or Blueprint or model .
+
+      For example:  Suppose , tumi akti car kinba. Car kinar jnno tumi showroom e geso . showroom e jawar por tmr 2 ta color er car 
+      posondo hoyese akta hosse Red color and arekta hosse Blue color. Ekhon car company to alada alada color er jonno alada alada model toiri 
+      korbe nh tai nh? ekhane "CAR" ta hosse class . ei class er modde tumi car jekono properties(color, model, speeed ,behaviour) add/change korte parba
+
+     
+     Syntax: class is a keyword
+
+     class  <className>  {
+     1. function()
+     2. method ()
+     3. constructor()
+     4. variables
+     }
+
+  2. Objects ::
+
+       Objects is an instance of a class...Without object the class can not memory allocated .
+
+       jemon amra int a; likhi to ata mane just a ke declaration kora but tokhon memory allocate hoy nh
+       jokhon amara kata value assign kori like int a=10; then 10 value a er modde assign hoy then memory allocate kore.
+
+       thik same vabe object cara class memory allocate hoy nh.
+
+       for example: 
+
+           class Name{
+           add(){
+           int a=10, b=10, c;
+           c = a+b;
+           print(c);
+
+           }
+           }
+
+
+           void main(){
+           Name obj = Name();    ///create object
+
+           obj.add();         // call function through object
+
+          
+           
+           }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */
