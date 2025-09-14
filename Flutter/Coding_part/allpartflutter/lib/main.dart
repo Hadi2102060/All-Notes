994c2ce843f2a06c0af3cbb0007ca821a61d9245
Hadi2102060 <ug2102060@cse.pstu.ac.bd>
2025-09-14 11:32:13 +0600

    update

---
+++ Flutter/Coding_part/allpartflutter/lib/main.dart
@@ -0,0 +1,40 @@
+//........................Inhereted widget...............
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(displayName: "Developer Hadi"),
+      theme: ThemeData(primaryColor: Colors.tealAccent),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  final String displayName;
+  const Home({super.key, required this.displayName});
+
+  @override
+  Widget build(BuildContext context) {
+    Color primaryColor = Theme.of(
+      context,
+    ).primaryColor; // ei primary color ta ami jekono jaygai use korte parbo
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: primaryColor,
+        title: Text("Inherited Widget"),
+      ),
+      //backgroundColor: primaryColor,
+      body: Center(child: Text("$displayName")),
+    );
+  }
+}
