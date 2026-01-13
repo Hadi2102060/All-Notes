931a260c5dc865120de3f938ac1a563317dcb636
Hadi2102060 <ug2102060@cse.pstu.ac.bd>
2026-01-13 23:16:14 +0600

    06 update- List_generate with List_seperated widget

--- Flutter/tutorial/lib/06_ListView_Deep_Dive.dart
+++ Flutter/tutorial/lib/06_ListView_Deep_Dive.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class SixthPage extends StatefulWidget {
   const SixthPage({super.key});
 
@@ -9,65 +8,206 @@ class SixthPage extends StatefulWidget {
 }
 
 class _SixthPageState extends State<SixthPage> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screen = [Basic(), Vertical(), Horizontal()];
+
+  void onTappedIndex(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Sixth Page (ListView Deep Dive)",style: TextStyle(
-          fontWeight: FontWeight.bold,
-          fontSize: 20,
-          color: Colors.white,
-        ),),
+        title: Text(
+          "Sixth Page (ListView Deep Dive)",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+            color: Colors.white,
+          ),
+        ),
         backgroundColor: Colors.teal,
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView(
-              children: [
-                ListTile(
-                  tileColor: Colors.teal.shade100,
-                  title: Text("Hadi"),
-                  subtitle: Text("Flutter Developer"),
-                  leading: CircleAvatar(
-                    backgroundColor: Colors.teal.shade300,
-                    backgroundImage: AssetImage("assets/hadi.jpg"),
-                  ),
-                  trailing: Icon(Icons.star, color: Colors.amber,),
-                ),
-                ListTile(
-                  tileColor: Colors.teal.shade200,
-                  title: Text("Yasin"),
-                  subtitle: Text("Data Scientist"),
-                ),
-                ListTile(
-                  tileColor: Colors.teal.shade300,
-                  title: Text("shuvo"),
-                  subtitle: Text("Programmer"),
-                ),
-              ],
-            ),
-          ),
-          SizedBox(height: 20),
-          Container(),
-          SizedBox(height: 20),
-          Container()
-        ],
-      ),
+      body: _screen[_currentIndex],
 
       bottomNavigationBar: BottomNavigationBar(
-        backgroundColor:Colors.blueGrey.shade900,
+        backgroundColor: Colors.blueGrey.shade900,
 
         selectedItemColor: Colors.orangeAccent,
 
         unselectedItemColor: Colors.blueGrey.shade300,
 
+        currentIndex: _currentIndex,
+        onTap: onTappedIndex,
+
         items: [
-          BottomNavigationBarItem(icon: Icon(Icons.menu_open,color: Colors.white,), label: "Basic",),
-          BottomNavigationBarItem(icon: Icon(Icons.text_rotate_vertical), label: "Vertical"),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.menu_open, color: Colors.white),
+            label: "Basic",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.text_rotate_vertical),
+            label: "Vertical",
+          ),
           BottomNavigationBarItem(icon: Icon(Icons.list), label: "Horizontal"),
-        ]
+        ],
+      ),
+    );
+  }
+}
+
+class Basic extends StatelessWidget {
+  const Basic({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          child: ListView(
+            children: [
+              ListTile(
+                tileColor: Colors.teal.shade100,
+                title: Text("Hadi"),
+                subtitle: Text("Flutter Developer"),
+                leading: CircleAvatar(
+                  backgroundColor: Colors.teal.shade300,
+                  backgroundImage: AssetImage("assets/hadi.jpg"),
+                ),
+                trailing: Icon(Icons.star, color: Colors.amber),
+              ),
+              ListTile(
+                tileColor: Colors.teal.shade200,
+                title: Text("Yasin"),
+                subtitle: Text("Data Scientist"),
+                trailing: Icon(Icons.star, color: Colors.amber),
+                leading: CircleAvatar(
+                  backgroundColor: Colors.teal.shade300,
+                  backgroundImage: AssetImage("assets/yasin.jpg"),
+                ),
+              ),
+              ListTile(
+                tileColor: Colors.teal.shade300,
+                title: Text("shuvo"),
+                subtitle: Text("Programmer"),
+                trailing: Icon(Icons.star, color: Colors.amber),
+                leading: CircleAvatar(
+                  backgroundColor: Colors.teal.shade300,
+                  backgroundImage: AssetImage("assets/shuvo.jpg"),
+                ),
+              ),
+            ],
+          ),
         ),
+        SizedBox(height: 20),
+        Container(),
+        SizedBox(height: 20),
+        Container(),
+      ],
+    );
+  }
+}
+
+class Vertical extends StatefulWidget {
+  const Vertical({super.key});
+
+  @override
+  State<Vertical> createState() => _VerticalState();
+}
+
+class _VerticalState extends State<Vertical> {
+  final items = List.generate(100, (counter) => "Item $counter");
+  @override
+  Widget build(BuildContext context) {
+    /*...............ekhaner SeparedBuilder er maddome custom divider line banano jay jemon dot dot line .........
+
+    // return ListView.separated(
+    //   separatorBuilder: (context, index) => Row(
+    //     children: List.generate(20, (context)=>
+    //       Expanded(child: Container(
+    //         margin: EdgeInsets.symmetric(horizontal: 7),
+    //         height: 1, 
+    //         color: const Color.fromARGB(255, 253, 252, 252),
+    //       )))
+
+    //   ),
+
+
+
+
+    //.........shudu gap chaile ei vabe korbe.............
+
+    // separatorBuilder: (context, index) {
+    //   return SizedBox(height: 15);
+    //    },
+
+
+
+    ................
+
+
+    */
+
+    return ListView.separated(
+      separatorBuilder: (context, index) => Divider(
+        height: 2, // prottekta item er modde 2 height er space dibe
+        thickness: 5, // divider line er purotto
+        indent: 20, // left theke divider line ta 20 space niye start hobe
+        endIndent: 20, // right theke divider line ta 20 space niye ses hobe
+        color: Colors.black,
+      ),
+      itemCount: items.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text("Item $index"),
+          leading: CircleAvatar(
+            backgroundColor: Colors.teal.shade300,
+            child: Text("$index"),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class Horizontal extends StatefulWidget {
+  const Horizontal({super.key});
+
+  @override
+  State<Horizontal> createState() => _HorizontalState();
+}
+
+class _HorizontalState extends State<Horizontal> {
+  final items = List.generate(100, (counter) => "Item $counter");
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      //
+      separatorBuilder: (context, index) {
+        return Container(
+          margin: EdgeInsets.symmetric(horizontal: 16),
+          height: 8,
+          decoration: BoxDecoration(
+            color: Colors.grey.shade200,
+            borderRadius: BorderRadius.circular(10),
+          ),
+        );
+      },
+
+      itemCount: items.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text("Item $index"),
+          leading: CircleAvatar(
+            backgroundColor: Colors.teal.shade300,
+            child: Text("$index"),
+          ),
+        );
+      },
     );
   }
 }
